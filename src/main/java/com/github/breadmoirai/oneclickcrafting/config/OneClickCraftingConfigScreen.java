ba6c8ca9d517bc0b29c613bb406950f9825b0f23
BreadMoirai <breadmoirai@gmail.com>
2025-06-18 22:55:14 -0700

    Update 1.21.6

--- src/main/java/com/github/breadmoirai/oneclickcrafting/config/OneClickCraftingConfigScreen.java
+++ src/main/java/com/github/breadmoirai/oneclickcrafting/config/OneClickCraftingConfigScreen.java
@@ -4,6 +4,7 @@ import dev.lambdaurora.spruceui.Position;
 import dev.lambdaurora.spruceui.SpruceTexts;
 import dev.lambdaurora.spruceui.option.SpruceBooleanOption;
 import dev.lambdaurora.spruceui.screen.SpruceScreen;
+import dev.lambdaurora.spruceui.tooltip.TooltipData;
 import dev.lambdaurora.spruceui.widget.SpruceButtonWidget;
 import dev.lambdaurora.spruceui.widget.container.SpruceOptionListWidget;
 import net.fabricmc.api.EnvType;
@@ -30,37 +31,37 @@ public class OneClickCraftingConfigScreen extends SpruceScreen {
               "config.oneclickcrafting.left_click",
               config::isEnableLeftClick,
               config::setEnableLeftClick,
-              Text.translatable("config.oneclickcrafting.left_click.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.left_click.tooltip")).build()
       );
       SpruceBooleanOption rightClick = new SpruceBooleanOption(
               "config.oneclickcrafting.right_click",
               config::isEnableRightClick,
               config::setEnableRightClick,
-              Text.translatable("config.oneclickcrafting.right_click.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.right_click.tooltip")).build()
       );
       SpruceBooleanOption alwaysOn = new SpruceBooleanOption(
               "config.oneclickcrafting.always_on",
               config::isAlwaysOn,
               config::setAlwaysOn,
-              Text.translatable("config.oneclickcrafting.always_on.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.always_on.tooltip")).build()
       );
       SpruceBooleanOption altHold = new SpruceBooleanOption(
               "config.oneclickcrafting.alt_hold",
               config::isAltHold,
               config::setAltHold,
-              Text.translatable("config.oneclickcrafting.alt_hold.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.alt_hold.tooltip")).build()
       );
       SpruceBooleanOption ctrlHold = new SpruceBooleanOption(
               "config.oneclickcrafting.ctrl_hold",
               config::isCtrlHold,
               config::setCtrlHold,
-              Text.translatable("config.oneclickcrafting.ctrl_hold.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.ctrl_hold.tooltip")).build()
       );
       SpruceBooleanOption dropEnable = new SpruceBooleanOption(
               "config.oneclickcrafting.drop_enable",
               config::isDropEnable,
               config::setDropEnable,
-              Text.translatable("config.oneclickcrafting.drop_enable.tooltip")
+              TooltipData.builder().text(Text.translatable("config.oneclickcrafting.drop_enable.tooltip")).build()
       );
 
       list.addOptionEntry(leftClick, rightClick);
